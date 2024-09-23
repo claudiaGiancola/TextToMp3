ffb5586ba7e2f9837b3e29f45b783ca8933de7ab
claudiaGiancola <cloemezzaluna@gmail.com>
2024-09-23 13:17:13 +0100

    README added

--- TextToMp3/obj/Debug/net8.0/TextToMp3.AssemblyInfo.cs
+++ TextToMp3/obj/Debug/net8.0/TextToMp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextToMp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb92dbd27001b36ca0d9560d0abe5c1ed63c30b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextToMp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextToMp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
