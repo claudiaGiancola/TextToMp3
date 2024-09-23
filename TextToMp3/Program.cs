cb92dbd27001b36ca0d9560d0abe5c1ed63c30b7
claudiaGiancola <cloemezzaluna@gmail.com>
2024-09-23 13:09:22 +0100

    using System.Speech.Synthesis, installation and example

---
+++ TextToMp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Speech.Synthesis;
+
+// Initialize a new instance of the SpeechSynthesizer.
+SpeechSynthesizer synth = new SpeechSynthesizer();
+
+// Configure the audio output.
+synth.SetOutputToDefaultAudioDevice();
+
+// Speak a string, synchronously
+synth.Speak("Hello World!");
+
+// Speak a string asynchronously
+var prompt = synth.SpeakAsync("Goodnight Moon!");
+
+while (!prompt.IsCompleted)
+{
+    Console.WriteLine("speaking...");
+    Thread.Sleep(500);
+}
