fe92497f6c34b8bfefd81d7848f8d3c8e885c362
claudiaGiancola <cloemezzaluna@gmail.com>
2024-09-24 20:02:56 +0100

    create separated BlazorWebApp and ConsoleApp versions

---
+++ TextToMp3_ConsoleApp/TextToMp3_ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Speech.Synthesis;
+
+string mp3FilePath = @"C:\Training\TextToMp3\TextToMp3_ConsoleApp\exportTests\MartelloDelSoleTest.mp3";
+
+using (SpeechSynthesizer synth = new SpeechSynthesizer())
+using (MemoryStream wavStream = new MemoryStream())
+{
+
+    // Configure the audio output
+    synth.SetOutputToWaveStream(wavStream);
+    // Input the text to be turned into speech
+    synth.SelectVoice("Microsoft Elsa Desktop");
+    synth.Speak(InputFile.GetInputFile());
+
+    // Reset the position of the stream to the beginning before conversion
+    wavStream.Position = 0;
+
+    // Convert WAV stream to MP3 and save it to file
+    WavToMp3.ConvertWavStreamToMp3(wavStream, mp3FilePath);
+
+}
+
+Console.WriteLine("MP3 file generated.");
+
+
+
